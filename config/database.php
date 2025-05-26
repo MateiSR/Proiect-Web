fc4fce8c8942ff5db132c567332fbd8e25a053b6
Matei Radu <MateiSR@proton.me>
2025-05-26 18:35:59 +0300

    Add basic database setu[

---
+++ config/database.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'books_db';
+    private $username = 'postgres';
+    private $password = 'postgres';
+    private $port = '5432';
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+
+        try {
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            // actually throw error so they can be caught
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // fetch as associative array (map column names to values)
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo 'Connection Error: ' . $e->getMessage();
+            exit;
+        }
+
+        return $this->conn;
+    }
+}
+?>
---
+++ database/init-scripts/init.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+COMMIT;
